a78870799554c32d289ac85e295d0964bc890f58
Khalin Redding <khalinredd@gmail.com>
2021-01-16 17:27:51 -0500

    commit tree d5d0f7076a8dbd2330958b3b6e8f0e4fa3ee62d2

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+require 'pry'
+class School
+
+    attr_accessor :roster, :name, :add_student
+
+    def initialize(name)
+
+        @name = name
+        @roster = {}
+        # @add_student = add_student
+    
+    end
+
+    def add_student(student, grade)
+
+        @roster = {grade=>student} 
+        # binding.pry
+        # # student_info = {student: grade}
+        # @roster << 
+    end
+
+
+
+
+end
+# binding.pry
